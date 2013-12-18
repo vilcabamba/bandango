5146fe3965b54f1b414d2b41460090ac63442e22
Mario Andrés Correa <admin@macool.me>
2013-12-18 14:48:48 -0500

    added User::Tokenizable, that creates user's token before saving new users to DB

---
+++ app/models/concerns/user/tokenizable.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  module Tokenizable
+    extend ActiveSupport::Concern
+
+    included do
+      before_create :create_token!
+    end
+
+    def create_token!
+      begin
+        self.token = SecureRandom.hex(50)
+      end while self.class.exists?(token: @token)
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   include Validations
+  include Tokenizable
 
 end
