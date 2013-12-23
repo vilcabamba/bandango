2c50b1f21159c809d0ac39a22349b7e659140f8a
Mario Andrés Correa <admin@macool.me>
2013-12-23 09:47:44 -0500

    returning mystery man as default image for gravatar if no email given. If email given, returns identicon as default image
    see https://en.gravatar.com/site/implement/images/#default-image

--- app/models/concerns/gravatar_imagen.rb
+++ app/models/concerns/gravatar_imagen.rb
@@ -1,5 +1,15 @@
 module GravatarImagen
   def imagen_url
-    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}" unless email.blank?
+    "http://www.gravatar.com/avatar/#{email_hexdigest_and_default[0]}?d=#{email_hexdigest_and_default[1]}"
+  end
+  def email_hexdigest_and_default
+    # returns the email hexdigest and the default image
+    # if user doesn't have email, returns 'a' and *mystery man* as default
+    # if user has email, it returns a hexdigest of the email and *identicon* as default image
+    @email_hexdigest_and_default ||= if email.blank?
+      ["a", "mm"]
+    else
+      [Digest::MD5.hexdigest(email), "identicon"]
+    end
   end
 end
