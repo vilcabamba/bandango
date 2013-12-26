86062cbe94b0a1e6c65ece42b1177666ab1368ee
Mario Andrés Correa <admin@macool.me>
2013-12-26 09:39:26 -0500

    added Sessions module and including it on ApplicationController

--- app/controllers/concerns/application_controller/sessions.rb
+++ app/controllers/concerns/application_controller/sessions.rb
@@ -1,9 +1,25 @@
 class ApplicationController < ActionController::Base
   module Sessions
     extend ActiveSupport::Concern
-    
-    module ClassMethods
-      
+
+    included do
+      helper_method :current_user
     end
+
+    protected
+
+    def current_user
+      @current_user ||= User.cached_find(session[:user_id]) if session[:user_id]
+    end
+    def confirm_logged_in
+      unless current_user
+        redirect_to root_url
+        false
+      end
+    end
+    def logout
+      session[:user_id] = nil
+    end
+
   end
 end
