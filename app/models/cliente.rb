c6bb91ba62399f68bd75bea89667c1cae1e2604c
Mario Andrés Correa <admin@macool.me>
2013-12-30 13:31:53 -0500

    annotated

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: clientes
+#
+#  id             :integer          not null, primary key
+#  tipo_id        :string(255)      default("Cédula"), not null
+#  identificacion :string(255)      not null
+#  nombres        :string(255)      not null
+#  direccion      :string(255)
+#  telefono       :string(255)
+#  email          :string(255)
+#  created_at     :datetime
+#  updated_at     :datetime
+#
+
 class Cliente < ActiveRecord::Base
   include Cacheable
   include Validations
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :integer          not null, primary key
+#  username         :string(255)      not null
+#  email            :string(255)
+#  crypted_password :string(255)
+#  salt             :string(255)
+#  created_at       :datetime
+#  updated_at       :datetime
+#  token            :string(255)
+#  nombres          :string(255)
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
