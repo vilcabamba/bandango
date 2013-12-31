4ad5d99984c5c17b55068bd9cea44cd05c607489
Mario Andrés Correa <admin@macool.me>
2013-12-31 15:37:14 -0500

    annotate and bugfix

--- app/models/comprobante.rb
+++ app/models/comprobante.rb
@@ -1,7 +1,18 @@
+# == Schema Information
+#
+# Table name: comprobantes
+#
+#  id                  :integer          not null, primary key
+#  codigo              :integer          not null
+#  tipo                :string(255)      not null
+#  codigo_secuenciales :text
+#  sustento_tributario :text
+#
+
 class Comprobante < ActiveRecord::Base
 
 # relationships
-  belongs_to :venta
+  has_many :ventas
 
 # serializations
   serialize :codigo_secuenciales, Array
--- app/models/venta.rb
+++ app/models/venta.rb
@@ -1,8 +1,25 @@
+# == Schema Information
+#
+# Table name: ventas
+#
+#  id             :integer          not null, primary key
+#  cliente_id     :integer          not null
+#  comprobante_id :integer          not null
+#  base_no_iva    :float            not null
+#  base_iva_zero  :float            not null
+#  base_iva       :float            not null
+#  monto_iva      :float            not null
+#  iva_retenido   :float            not null
+#  renta_retenido :float            not null
+#  created_at     :datetime
+#  updated_at     :datetime
+#
+
 class Venta < ActiveRecord::Base
   self.table_name = :ventas
 
 # relationships
   belongs_to :cliente
-  has_many :comprobantes
+  belongs_to :comprobante
 
 end
