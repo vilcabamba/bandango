80a5e3312b646f5bab4c40f7b0fb724ca923e811
Mario Andrés Correa <admin@macool.me>
2014-01-08 14:43:38 -0500

    these fields doesn't exist anymore

--- app/serializers/venta_serializer.rb
+++ app/serializers/venta_serializer.rb
@@ -1,12 +1,6 @@
 class VentaSerializer < ActiveModel::Serializer
 
   attributes :id,
-             :base_no_iva,
-             :base_iva_zero,
-             :base_iva,
-             :monto_iva,
-             :iva_retenido,
-             :renta_retenido,
              :created_at
 
   has_one :cliente, key: :cliente_id, embed: :ids, include: true
