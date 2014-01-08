7d7799e9dfaf62b07cbd808e86992cc334c2afde
Mario Andrés Correa <admin@macool.me>
2014-01-08 14:38:07 -0500

    these fields doesn't exist anymore

--- app/models/concerns/venta/validations.rb
+++ app/models/concerns/venta/validations.rb
@@ -5,16 +5,6 @@ class Venta < ActiveRecord::Base
     included do
       validates :cliente_id,     presence: true
       validates :comprobante_id, presence: true
-      validates :base_no_iva,    presence: true,
-                                 numericality: { greater_than: 0 }
-      validates :base_iva_zero,  presence: true,
-                                 numericality: { greater_than: 0 }
-      validates :base_iva,       presence: true,
-                                 numericality: { greater_than: 0 }
-      validates :monto_iva,      presence: true,
-                                 numericality: { greater_than: 0 }
-      validates :iva_retenido,   presence: true,
-                                 numericality: { greater_than: 0 }
     end
   end
 end
