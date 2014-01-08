0b11662a07f497228643c385278a4c6967fb8874
Mario Andrés Correa <admin@macool.me>
2014-01-08 15:46:52 -0500

    doing Api::VentasController

--- app/controllers/api/ventas_controller.rb
+++ app/controllers/api/ventas_controller.rb
@@ -8,6 +8,25 @@ module Api
       @ventas = Venta.includes(:cliente, :comprobante).page(params[:page])
       render json: @ventas, meta: { total_pages: @ventas.total_pages, page: @ventas.current_page }
     end
+
+    def show
+      respond_with Venta.cached_find(params[:id])
+    end
+
+    def create
+      respond_with :api, Venta.create(venta_params)
+    end
+
+    private
+
+    def venta_params
+      params.require(:venta).permit :cliente_id,
+                                    :comprobante_id,
+                                    :order_items => [
+                                      :cantidad,
+                                      :item_id
+                                    ]
+    end
     
   end
 end
