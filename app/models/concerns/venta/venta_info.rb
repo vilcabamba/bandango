6ca0b8d911ee4c12623c124591fca2722b5ae566
Mario Andrés Correa <admin@macool.me>
2014-01-08 14:37:32 -0500

    added Venta::VentaInfo
    methods not defined yet

---
+++ app/models/concerns/venta/venta_info.rb
@@ -0,0 +1,15 @@
+class Venta < ActiveRecord::Base
+  module VentaInfo
+
+    def base_no_iva
+    end
+    def base_iva_zero
+    end
+    def base_iva
+    end
+    def monto_iva
+    end
+    def iva_retenido
+    end
+  end
+end
