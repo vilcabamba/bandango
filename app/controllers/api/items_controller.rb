3a84739d9db5a763ba509631fd0c73e6200a9632
Mario Andrés Correa <admin@macool.me>
2014-01-09 11:05:38 -0500

    Api::ItemsController now allows #show, #create, #update

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -16,5 +16,30 @@ module Api
       end
     end
 
+    def show
+      respond_with Item.cached_find(params[:id])
+    end
+
+    def create
+      respond_with :api, Item.create(item_params)
+    end
+
+    def update
+      respond_with Item.update(params[:id], item_params)
+    end
+
+    private
+
+    def item_params
+      params.require(:item).permit :nombre,
+                                   :descripcion,
+                                   :base,
+                                   :iva,
+                                   :iva_tarifa,
+                                   :ice,
+                                   :ice_tarifa,
+                                   :category_id
+    end
+
   end
 end
