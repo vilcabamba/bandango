90ea9e508e69c6f57cb96053b4a39d107f099b31
Mario Andrés Correa <admin@macool.me>
2014-01-10 13:26:33 -0500

    annotate and seeds for ConceptoRetencionFuente

--- app/models/concepto_retencion_fuente.rb
+++ app/models/concepto_retencion_fuente.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: concepto_retencion_fuentes
+#
+#  id         :integer          not null, primary key
+#  codigo     :string(255)      not null
+#  concepto   :string(255)      not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class ConceptoRetencionFuente < ActiveRecord::Base
 end
