87e1be2574cc7a3538722a8763f2085127d07f90
Mario Andrés Correa <admin@macool.me>
2014-01-10 13:26:20 -0500

    precompile of assets should be on production as well

--- update.bash
+++ update.bash
@@ -7,5 +7,5 @@ bundle install --deployment
 bundle exec rake db:migrate RAILS_ENV=production
 # MongoDB indexes:
 # rake db:mongoid:create_indexes RAILS_ENV=production
-bundle exec rake assets:precompile
+bundle exec rake assets:precompile RAILS_ENV=production
 touch tmp/restart.txt
