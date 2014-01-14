e1c87cc8a1ae4256a57ce6688216355b786bd26b
Mario Andrés Correa <admin@macool.me>
2014-01-13 21:50:48 -0500

    request params should be inside `transaccion`

--- app/workers/sani_post_worker.rb
+++ app/workers/sani_post_worker.rb
@@ -5,10 +5,12 @@ class SaniPostWorker
   def perform(object, class_name, action)
     url = SANI[:host] + "/api/transacciones.json"
     params = {
-      action: action,
-      class_name: class_name,
-      object: object
+      transaccion: {
+        action: action,
+        class_name: class_name,
+        object: object
+      }
     }
-    response = RestClient.post url, params.to_json, :content_type => :json, :Authorization => "Token token=\"#{SANI[:token]}\""
+    RestClient.post url, params.to_json, :content_type => :json, :Authorization => "Token token=\"#{SANI[:token]}\""
   end
 end
