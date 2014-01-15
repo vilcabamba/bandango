43ad6767b5a9c3d8f51e79bcf7643e95173c6d4c
Mario Andrés Correa <admin@macool.me>
2014-01-14 23:33:33 -0500

    now scheduling one SaniGetWorker at application startup

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,6 +1,10 @@
 key = YAML::load_file(File.join(Rails.root.to_s, "config", "pusher.yml"))["key"]
 
+# set rails logger
 PusherClient.logger = Rails.logger
+
+# schedule 1 get
+SaniGetWorker.schedule
 
 socket = PusherClient::Socket.new(key)
 socket.connect(true) # Connect asynchronously
