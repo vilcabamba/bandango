b2776b0c55e5a796a6f30552effec44c4565ac2e
Mario Andrés Correa <admin@macool.me>
2014-01-15 10:09:34 -0500

    removed comments

--- script/update.bash
+++ script/update.bash
@@ -1,12 +1,7 @@
 #!/bin/bash
-# git version:
 git pull
-# svn version:
-#svn update
 bundle install --deployment
 bundle exec rake db:migrate RAILS_ENV=production
 bundle exec rake db:seed RAILS_ENV=production
-# MongoDB indexes:
-# rake db:mongoid:create_indexes RAILS_ENV=production
 bundle exec rake assets:precompile RAILS_ENV=production
 touch tmp/restart.txt
