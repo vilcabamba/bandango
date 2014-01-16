de0ebb499df8c1baf40844c414aed1817cc10343
Mario Andrés Correa <admin@macool.me>
2014-01-16 12:21:00 -0500

    update script now specifies crontab to keep updated

--- script/update.bash
+++ script/update.bash
@@ -4,4 +4,5 @@ bundle install --deployment
 bundle exec rake db:migrate RAILS_ENV=production
 bundle exec rake db:seed RAILS_ENV=production
 bundle exec rake assets:precompile RAILS_ENV=production
+bundle exec whenever --update-crontab
 touch tmp/restart.txt
