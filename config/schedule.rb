054152ccd9448b1127b6851b7287eef09600d138
Mario Andrés Correa <admin@macool.me>
2014-01-17 02:15:10 -0500

    updated schedule (still testing)

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,13 +3,26 @@
 job_type :bandango, "cd /home/pi/bandango && :task"
 
 # every 1.hour do
-every :day, :at => '01:00pm' do
-  bandango "git pull"
-  bandango "bundle install --deployment"
+every 15.minutes do
+  runner "SaniGetWorker.schedule"
+end
+
+every :day, :at => "03:00am" do
+  bandango "git pull && bundle install --deployment"
+end
+every :day, :at => "03:10am" do
   rake "db:migrate"
+end
+every :day, :at => "03:20am" do
   rake "db:seed"
+end
+every :day, :at => "03:30am" do
   rake "assets:precompile"
+end
+every :day, :at => "04:00am" do
   command "sudo service bandango restart"
   command "sudo service sidekiq stop"
+end
+every :day, :at => "04:10am" do
   command "sudo service sidekiq start"
 end
