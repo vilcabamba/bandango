b192140dbf6180fa3f0e00d13b1b61777d3331a1
Mario Andrés Correa <admin@macool.me>
2014-01-17 18:39:40 -0500

    irregular of "emisor"

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -7,6 +7,7 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
   inflect.irregular 'venta', 'ventas'
+  inflect.irregular 'emisor', 'emisores'
 #   inflect.uncountable %w( fish sheep )
 end
 
