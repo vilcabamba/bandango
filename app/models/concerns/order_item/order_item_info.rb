527663a23d6f65661f91d475eb634e6b63889c8b
Fabricio Flores <fabrifloresg@gmail.com>
2014-01-17 21:03:14 -0500

    change base to base_venta when tipo is venta

--- app/models/concerns/order_item/order_item_info.rb
+++ app/models/concerns/order_item/order_item_info.rb
@@ -3,13 +3,16 @@ class OrderItem < ActiveRecord::Base
     extend ActiveSupport::Concern
 
     included do
+      attr_accessor :tipo
       before_validation :set_item_attributes
     end
 
     def set_item_attributes
       if item
-        self.cached_item_precio = item.base
         self.cached_item_nombre = item.nombre
+        if tipo == Venta
+          self.cached_item_precio = item.base_venta
+        end
       end
     end
   end
