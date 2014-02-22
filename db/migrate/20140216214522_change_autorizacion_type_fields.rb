57eb0c9923f5943514b9915ca9e18578523f9572
Mario Andrés Correa <admin@macool.me>
2014-02-22 15:04:10 -0500

    fix migration
    these columns already exist at this point

--- db/migrate/20140216214522_change_autorizacion_type_fields.rb
+++ db/migrate/20140216214522_change_autorizacion_type_fields.rb
@@ -1,6 +1,6 @@
 class ChangeAutorizacionTypeFields < ActiveRecord::Migration
   def change
-    add_column :compras, :autorizacion_comprobante, :string
-    add_column :compras, :numero_autorizacion_comprobante_modificado, :string
+    change_column :compras, :autorizacion_comprobante, :string
+    change_column :compras, :numero_autorizacion_comprobante_modificado, :string
   end
 end
