de67dab9d42f6ea13379eea967e1aae8d7b5a675
Mario Andrés Correa <admin@macool.me>
2014-02-22 23:36:44 -0500

    differ sidekiq's namespace across environments

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => 'redis://localhost:6379', :namespace => Rails.application.class.parent_name }
+  config.redis = { url: "redis://localhost:6379", namespace: "#{Rails.application.class.parent_name}_#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => 'redis://localhost:6379', :namespace => Rails.application.class.parent_name }
+  config.redis = { url: "redis://localhost:6379", namespace: "#{Rails.application.class.parent_name}_#{Rails.env}" }
 end
