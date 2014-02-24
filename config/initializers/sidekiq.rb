797277f492334e4216022905b6dbe8513db31db8
Mario Andrés Correa <admin@macool.me>
2014-02-24 00:42:29 -0500

    sidekiq support for Phusion passenger

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,17 @@
+base_conf = { url: "redis://localhost:6379", namespace: "#{Rails.application.class.parent_name}_#{Rails.env}" }
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://localhost:6379", namespace: "#{Rails.application.class.parent_name}_#{Rails.env}" }
+  config.redis = base_conf
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://localhost:6379", namespace: "#{Rails.application.class.parent_name}_#{Rails.env}" }
+  config.redis = base_conf.merge(size: 1)
+end
+
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    Sidekiq.configure_client do |config|
+      config.redis = base_conf.merge(size: 1)
+    end if forked
+  end
 end
