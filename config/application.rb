5f4395df4c7fa0df57c3d9905ebd7a7da62f4b1d
Mario Andrés Correa <admin@macool.me>
2014-03-05 16:15:32 -0500

    rake logger was logging time twice

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Rake
     alias_method :origin_invoke, :invoke if method_defined?(:invoke)
     def invoke(*args)
       logger = Logger.new("log/rake.log")
-      logger.info "#{Time.now} -- #{name}"
+      logger.info name
       origin_invoke(*args)
     end
   end
