b117452aefac6b3b6b9d3fecfdf3a413427b0a66
Mario Andrés Correa <a@macool.me>
2014-03-23 15:32:38 -0500

    be more strict with routes

--- config/routes.rb
+++ config/routes.rb
@@ -55,18 +55,22 @@ Bandango::Application.routes.draw do
   namespace :api do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
-    resources :stats, only: [:index]
     resources :clientes
     resources :users
     resources :ventas
     resources :compras
-    resources :comprobantes
-    resources :sustento_comprobantes
-    resources :concepto_retencion_fuentes
     resources :items
     resources :categories
     resources :emisores
     resources :cierres_caja
+
+    resources :stats, only: :index
+
+    resources :sustento_comprobantes, only: :index
+    resources :concepto_retencion_fuentes, only: :index
+    resources :comprobantes, only: :index
+    resources :cash_denominations, only: :index
+
     resources :current_user do
       collection do
         post "/", to: "current_user#update"
