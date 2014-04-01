faee91c97cdd1c64383bac130d67c0ebf0b29da8
Mario Andrés Correa <a@macool.me>
2014-03-31 20:09:49 -0500

    email configuration is now read from .env

--- config/initializers/email_configuration.rb
+++ config/initializers/email_configuration.rb
@@ -1,1 +1,9 @@
-ActionMailer::Base.smtp_settings = YAML::load_file(File.join(Rails.root.to_s, "config", "email.yml")).symbolize_keys
+ActionMailer::Base.smtp_settings = {
+  address: ENV["SMTP_ADDRESS"] || "smtp.gmail.com",
+  port: ENV["SMTP_PORT"] || 587,
+  authentication: ENV["SMTP_AUTHENTICATION"] || "plain",
+  enable_starttls_auto: true,
+  domain: ENV["SMTP_DOMAIN"],
+  user_name: ENV["SMTP_USER_NAME"],
+  password: ENV["SMTP_PASSWORD"]
+}
