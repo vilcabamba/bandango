6dd0329f98167920c00981fff397bfcb50e165d7
Mario Andrés Correa <a@macool.me>
2014-04-07 23:12:01 -0500

    let err bit handle js errors

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,7 @@
 Airbrake.configure do |config|
-  config.api_key = "4ed831a9c49fedf639b3d68d90cd283b"
-  config.host    = "pangi.shiriculapo.com"
-  config.port    = 80
-  config.secure  = config.port == 443
+  config.api_key     = "4ed831a9c49fedf639b3d68d90cd283b"
+  config.host        = "pangi.shiriculapo.com"
+  config.port        = 80
+  config.secure      = config.port == 443
+  config.js_notifier = true
 end
