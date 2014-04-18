3a3e522b0455afdde38fc229b8ddef617561f8f8
Mario Andrés <a@macool.me>
2014-04-18 15:21:47 -0500

    minor refactor to `BandangoPusher`

--- lib/bandango_pusher.rb
+++ lib/bandango_pusher.rb
@@ -7,18 +7,8 @@ class BandangoPusher
     @minutes_to_reconnect = 1
     load_pusher_credentials
     set_pusher_logger
-    schedule_sani_workers
   end
-
-  def load_pusher_credentials
-    @key = ENV["PUSHER_KEY"]
-    @secret = ENV["PUSHER_SECRET"]
-  end
-
-  def schedule_sani_workers
-    SaniGetWorker.schedule
-  end
-
+  
   def initialize!
     @socket = PusherClient::Socket.new(@key, secret: @secret)
 
@@ -27,25 +17,33 @@ class BandangoPusher
 
     # Bind to a global event
     @socket.bind("new_transaccion") do |data|
-      Rails.logger.debug "new_transaction"
-      SaniGetWorker.schedule
+      schedule_sani_worker
     end
 
     connect!
+  end
+  
+  private
+
+  def load_pusher_credentials
+    @key = ENV["PUSHER_KEY"]
+    @secret = ENV["PUSHER_SECRET"]
+  end
+
+  def schedule_sani_worker
+    SaniGetWorker.schedule
   end
 
   def connect!
     @socket.connect
   rescue SocketError => e
     @minutes_to_reconnect *= 4
-    Rails.logger.info "Can't connect to pusher. Probably the internet connection is down. Will try again in #{minutes_to_reconnect}"
-    Rails.logger.debug "#{e} minutes"
+    Rails.logger.info "Can't connect to pusher (#{e}). Probably the internet connection is down. Will try again in #{minutes_to_reconnect}"
     sleep 60 * minutes_to_reconnect
     connect!
   end
 
   def set_pusher_logger
     PusherClient.logger = Rails.logger
-    PusherClient.logger.level = Logger::ERROR if Rails.env.production?
   end
 end
