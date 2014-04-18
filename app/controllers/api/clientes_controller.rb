d69dcc95cecbc98b84bf3502878e31316248e61c
Mario Andrés <a@macool.me>
2014-04-18 15:34:00 -0500

    hide private methods

--- app/controllers/api/clientes_controller.rb
+++ app/controllers/api/clientes_controller.rb
@@ -14,22 +14,6 @@ module Api
       end
     end
 
-    def search_by_identificacion
-      respond_with Cliente.cached_search_for_identificacion(params[:identificacion])
-    end
-
-    def search
-      render json: Cliente.sorted.search(params[:search]).limit(10),
-             meta: { query: params[:search] }
-    end
-
-    def list
-      clientes = Cliente.sorted.page(params[:page])
-      render json: clientes,
-             meta: { total_pages: clientes.total_pages,
-                            page: clientes.current_page }
-    end
-
     def show
       respond_with Cliente.cached_find(params[:id])
     end
@@ -52,6 +36,22 @@ module Api
     end
 
     private
+    
+    def search_by_identificacion
+      respond_with Cliente.cached_search_for_identificacion(params[:identificacion])
+    end
+
+    def search
+      render json: Cliente.sorted.search(params[:search]).limit(10),
+             meta: { query: params[:search] }
+    end
+
+    def list
+      clientes = Cliente.sorted.page(params[:page])
+      render json: clientes,
+             meta: { total_pages: clientes.total_pages,
+                            page: clientes.current_page }
+    end
 
     def cliente_params
       params.require(:cliente).permit :tipo_id, :identificacion, :nombres, :direccion, :telefono, :email
